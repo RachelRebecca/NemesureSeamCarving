236f9dfa7dfe68db377343d93a9bd4fe88e6bd96
RachelRebecca <rachelrebecca67@gmail.com>
2022-12-14 20:15:13 -0500

    modified Pixel class

--- src/main/java/seam/Pixel.java
+++ src/main/java/seam/Pixel.java
@@ -4,13 +4,16 @@ import java.awt.*;
 
 public class Pixel
 {
+    private final int maxEnergy =  255 * 255 * 6;
     private Color color;
     private double energy;
+    private double verticalEnergy;
+    private double horizontalEnergy;
 
     public Pixel(int rgb)
     {
         this.color = new Color(rgb);
-        this.energy = 255 * 255 * 6;
+        this.energy = maxEnergy;
     }
 
     public Color getColor()
@@ -28,4 +31,23 @@ public class Pixel
         this.energy = energy;
     }
 
+    public double getVerticalEnergy()
+    {
+        return verticalEnergy;
+    }
+
+    public void setVerticalEnergy(double verticalEnergy)
+    {
+        this.verticalEnergy = verticalEnergy;
+    }
+
+    public double getHorizontalEnergy()
+    {
+        return horizontalEnergy;
+    }
+
+    public void setHorizontalEnergy(double horizontalEnergy)
+    {
+        this.horizontalEnergy = horizontalEnergy;
+    }
 }
